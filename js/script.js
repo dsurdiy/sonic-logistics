6f93bb253f7c72073c054d131a7df4765bbc1887
DimaSurdiy <dsurdiy@ukr.net>
2024-04-23 21:24:52 +0300

    added adapted header with burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict"
+
+document.addEventListener("click", actions);
+
+function actions(e) {
+   const targetEl = e.target;
+
+   if (targetEl.closest(".menu-icon")) {
+      document.body.classList.toggle("menu-open");
+   }
+}
